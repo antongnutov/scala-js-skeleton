9adba1bb1e768986a67b40b988a74355780465e3
antongnutov <anton.gnutov@yandex.tu>
2017-04-07 13:26:02 +0300

    versions updated

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object Versions {
-  val http4s = "0.15.7a"
+  val http4s = "0.15.8a"
 
   val scalaTags = "0.6.3"
 
   val config = "1.3.1"
-  val log4j = "2.8"
+  val log4j = "2.8.2"
   val scalatest = "3.0.1"
 }
